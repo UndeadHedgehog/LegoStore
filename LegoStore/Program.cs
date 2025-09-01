bb36056aad2a4d39cedd1e2e91202f15c6aef676
Undead Hedgehog <undeadhedgehohgmc@gmail.com>
2025-09-01 15:04:22 +0200

    Initial commit

---
+++ LegoStore/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LegoStoreApi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args); // requires Microsoft.AspNetCore.App
+
+        builder.Services.AddControllers();
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
