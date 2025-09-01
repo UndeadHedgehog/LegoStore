657712262f73d400a105a4fdf108f3867698db94
Undead Hedgehog <undeadhedgehohgmc@gmail.com>
2025-09-01 17:38:58 +0200

    docs: How things  work comments, commit prefix file
    Added comments what methods roughly do, mainly for educational and bad memory purposes
    Added file storing prefixes for commits for better understanding for other people ( or for me with my dementia )

--- LegoStore/Program.cs
+++ LegoStore/Program.cs
@@ -8,14 +8,26 @@ class Program
     static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args); // requires Microsoft.AspNetCore.App
+        /* https://blog.stackademic.com/understanding-webapplication-createbuilder-args-in-asp-net-core-2bd63c76f438
+         * builder allows to:
+         *  - add Dependencies, Services, Components to app
+         *  - add and modify middleware
+         *  - app settings such as: connectionString, authentication providers, logging settings
+         */
 
         builder.Services.AddControllers();
-        var app = builder.Build();
+        /* From my understanding:
+         *  - registers definitions of each class that extends Controller / ControllerBase classes 
+         *  - By default, on every API request creates instance of specific controller class which dies after request is fully finished
+         *  - provides services ( things like [HttpGet], [Route], json stuff, and other things commonly used.
+         */
+        
+        var app = builder.Build(); // application that includes of the configurations added to previous WebApplicationBuilder
 
         app.UseHttpsRedirection();
+        
+        app.MapControllers(); // creates end-points for mine Controllers 
 
-        app.MapControllers();
-
-        app.Run();
+        app.Run(); // keeps app-thread running and listens for upcoming requests
     }
 }
